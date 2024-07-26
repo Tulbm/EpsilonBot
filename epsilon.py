01c24365a7b9a89fd3c358246be8c10542ceb703
Tulio Martins <batistatul9@gmail.com>
2024-07-26 00:02:40 -0400

    starting to play the game

--- epsilon.py
+++ epsilon.py
@@ -16,10 +16,41 @@ class epsilon(BotAI):  #base bot
    async def on_step(self, iteration: int): 
       print(f"iteration #{iteration}")
 
+      await self.distribute_workers() #idle workers
+      if self.townhalls: #at least one nexus
+         nexus = self.townhalls.random  
+         if nexus.is_idle and self.can_afford(UnitTypeId.PROBE):
+             nexus.train(UnitTypeId.PROBE)
+             
+         elif self.supply_left < 3 and self.already_pending(UnitTypeId.PYLON) == 0: #pylons
+            if self.can_afford(UnitTypeId.PYLON):
+               if self.structures(UnitTypeId.PYLON).amount == 0:  #first pylon
+                  await self.build(UnitTypeId.PYLON,near=nexus)  
+               else:
+                  target_pylon = self.structures(UnitTypeId.PYLON).closest_to(self.enemy_start_locations[0]) #pylon closes to the enemy
+                  pos = target_pylon.position.towards(self.enemy_start_locations[0], random.randrange(8, 15))
+                  await self.build(UnitTypeId.PYLON, near=pos)
+
+         elif not self.structures(UnitTypeId.GATEWAY):  
+            if self.can_afford(UnitTypeId.GATEWAY):
+               target_pylon = self.structures(UnitTypeId.PYLON).random
+               await self.build(UnitTypeId.GATEWAY,near=target_pylon)
+
+         elif self.structures(UnitTypeId.GATEWAY)      
+
+      else: #if no nexus
+         if self.can_afford(UnitTypeId.NEXUS): 
+                await self.expand_now()  
+      
+
+
+
+
 run_game(
    maps.get("AbyssalReefLE"),
    [Bot(Race.Protoss, epsilon()),
     Computer(Race.Zerg, Difficulty.Easy)],
     realtime=False,
 )
+
 
